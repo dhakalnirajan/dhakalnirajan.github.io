a5d5960fef7abfc9dca9d55cddfc12f7801765e8
Nirajan Dhakal <sorcerersupreme010@gmail.com>
2023-12-22 11:25:59 +0545

    xml file uploaded

--- js/theme.js
+++ js/theme.js
@@ -8,3 +8,20 @@ document.addEventListener ('DOMContentLoaded', function () {
     body.classList.toggle ('dark-theme');
   });
 });
+
+// Hamburger Style Navbar
+
+// Add click event listener to the hamburger menu icon
+document
+  .getElementById ('hamburger-icon')
+  .addEventListener ('click', function () {
+    // Toggle the visibility of the regular menu items
+    document.querySelector ('.nav-address').classList.toggle ('show-menu');
+  });
+
+// Add a click event listener to each menu item to hide the menu when clicked
+document.querySelectorAll ('.nav-address a').forEach (function (item) {
+  item.addEventListener ('click', function () {
+    document.querySelector ('.nav-address').classList.remove ('show-menu');
+  });
+});
